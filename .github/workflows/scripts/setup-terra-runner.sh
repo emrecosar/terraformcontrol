5251bad6d1f5fb9a95c39416c0b9e45d66772e42
Emre <emrecosarr@gmail.com>
2021-10-25 14:55:36 +0300

    Automation: added echo and sudo for testing pusposes

--- .github/workflows/scripts/setup-terra-runner.sh
+++ .github/workflows/scripts/setup-terra-runner.sh
@@ -5,7 +5,7 @@ TG_VERSION=$(cat terraform/.terragrunt-version)
 
 echo "after setting tf and tg versions"
 
-sudo tfenv install "${TF_VERSION}" && tfenv use "${TF_VERSION}"
-sudo tgenv install "${TG_VERSION}" && tgenv use "${TG_VERSION}"
+tfenv install "${TF_VERSION}" && tfenv use "${TF_VERSION}"
+tgenv install "${TG_VERSION}" && tgenv use "${TG_VERSION}"
 
 echo "after installing tfenv and tgenv"
